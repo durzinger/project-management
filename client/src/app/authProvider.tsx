83c051b57568870a6362339638702ce606d2603e
durzinger <mikkomanninen79@gmail.com>
2025-01-24 10:49:07 +0200

    added sign in sign up form

---
+++ client/src/app/authProvider.tsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { Authenticator, Placeholder } from '@aws-amplify/ui-react'
+import { Amplify } from 'aws-amplify'
+import "@aws-amplify/ui-react/styles.css"
+
+Amplify.configure({
+    Auth: {
+        Cognito: {
+            userPoolId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID || "",
+            userPoolClientId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_CLIENT_ID || "",
+        }
+    }
+});
+
+const formFields = {
+    signUp: {
+        username: {
+            order: 1,
+            placeholder: "Choose a username",
+            label: "Username",
+            isRequired: true
+        },
+        email: {
+            order: 2,
+            placeholder: "Enter your email address",
+            label: "Email",
+            isRequired: true
+        },
+        password: {
+            order: 3,
+            placeholder: "Enter your password",
+            label: "Password",
+            isRequired: true
+        },
+        confirm_password: {
+            order: 4,
+            placeholder: "Confirm your password",
+            label: "Confirm Password",
+            isRequired: true
+        },
+    }
+}
+
+const AuthProvider = ({ children}: any) => {
+    return (
+        <div className="mt-5">
+            <Authenticator formFields={formFields}>
+                {({ user }: any) => 
+                    user ? (
+                        <div>{children}</div>
+                    ) : (
+                        <h1>Please sign in below:</h1>
+                    )
+                }
+            </Authenticator>
+        </div>
+    )
+}
+
+export default AuthProvider
