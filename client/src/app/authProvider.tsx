cd54b3f02b10c0921d2672973fa67f40b77595fa
durzinger <mikkomanninen79@gmail.com>
2025-01-26 22:56:46 +0200

    css styling final changes

--- client/src/app/authProvider.tsx
+++ client/src/app/authProvider.tsx
@@ -43,7 +43,7 @@ const formFields = {
 
 const AuthProvider = ({ children}: any) => { // eslint-disable-line @typescript-eslint/no-explicit-any
     return (
-        <div className="mt-5">
+        <div>
             <Authenticator formFields={formFields}>
                 {({ user }: any) => // eslint-disable-line @typescript-eslint/no-explicit-any
                     user ? (
